ec64da6cf86e78cd27a13a4d0b8a48f12e0e6a96
Neeshu Sharma <neeshu.sharma@intellectdesign.com>
2019-01-20 17:12:31 +0530

    implemented double method without scale parameter.

--- src/main/java/cucumber_practice/Calculator.java
+++ src/main/java/cucumber_practice/Calculator.java
@@ -5,11 +5,22 @@ import java.math.BigDecimal;
 public class Calculator {
 
 	public int add(int a, int b) {
-		return a+b;
+		return a + b;
 	}
 
 	public double add(double a, double b, int scale) {
-		return new BigDecimal(a+b).setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+		return new BigDecimal(a + b).setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+
+	public double add(double a, double b) {
+		int scale = getNumberOfDecimal(a) > getNumberOfDecimal(b) ? getNumberOfDecimal(a) : getNumberOfDecimal(b);
+		return add(a, b, scale);
+	}
+
+	private int getNumberOfDecimal(double a) {
+		String text = Double.toString(Math.abs(a));
+		int integerPlaces = text.indexOf('.');
+		return text.length() - integerPlaces - 1;
 	}
 
 }
--- src/test/java/cucumber_practice/CalculatorTest.java
+++ src/test/java/cucumber_practice/CalculatorTest.java
@@ -5,29 +5,29 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	Calculator calculator = new Calculator();
-	
+
 	@Test
 	public void testAddIntergers() {
 		int a = 10;
 		int b = 20;
 		int c = calculator.add(a, b);
-		assert(c == 30);
+		assert (c == 30);
 	}
+
 	/**
-	 * send scale along with numbers.
-	 * Client is upset. He don't know what scale is.
-	 * He won't give you ####scale####.
+	 * send scale along with numbers. Client is upset. He don't know what scale is.
+	 * He won't give you ####scale####. He says give me decimals equals to maximum
+	 * decimal in any number.
 	 */
 	@Test
 	public void testAddDouble() {
 		double a = 10.3;
 		double b = 20.6;
 		double c = calculator.add(a, b);
-		assert(c == 30.9);
+		assert (c == 30.9);
 		a = 10.03;
 		b = 20.06;
-		c =  calculator.add(a, b);
-		assert(c == 30.09);
+		c = calculator.add(a, b);
+		assert (c == 30.09);
 	}
 }
-
