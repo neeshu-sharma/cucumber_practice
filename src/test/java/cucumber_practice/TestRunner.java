de5472e353e53813d7c826a06d0f2af12ffdd0b4
Neeshu Sharma <neeshu.sharma@intellectdesign.com>
2019-01-20 20:28:53 +0530

    initial cucumber setup

---
+++ src/test/java/cucumber_practice/TestRunner.java
@@ -0,0 +1,14 @@
+package cucumber_practice;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "feature"
+		,glue={"stepDefinition"}
+		)
+public class TestRunner {
+
+}
